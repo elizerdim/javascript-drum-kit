bf148b9fac253c2bfc265ae05b7115f6465be46e
Eliz Erdim <elizerdim@gmail.com>
2024-03-04 01:43:20 +0300

    Add 'click' functionality

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ window.addEventListener('keydown', playSound);
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+keys.forEach(key => key.addEventListener('click', playSound));
 
 function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.code}"]`);
-    const key = document.querySelector(`article[data-key="${e.code}"]`);
-    
+
+    //get data-key value from 'keydown' event and 'click' event
+    //go to parent node to get the value when children are clicked
+    const dataKey = 
+        e.code || 
+        e.target.getAttribute("data-key") || 
+        e.target.parentNode.getAttribute("data-key");
+
+    const audio = document.querySelector(`audio[data-key="${dataKey}"]`);
+    const key = document.querySelector(`article[data-key="${dataKey}"]`);
+
     if (!audio) return;
 
     key.classList.add('playing');
